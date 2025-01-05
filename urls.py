df378844eaaa0c60a1c0bb7a5383ceffe4bb1ec9
marwadiab <marwadi@ac.sce.ac.il>
2025-01-05 18:23:50 +0200

    chang

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # الصفحة الرئيسية
-    path('Student/', views.LogIn_Student, name='Student'),  # تسجيل دخول الطالب
-    path('Teacher/', views.LogIN_M, name='Teacher'),  # تسجيل دخول المعلم
-    path('Dean-of-Student-office/', views.LogInDeanStu, name='Dean_of_Student_office')  # تسجيل دخول مكتب عمادة شؤون الطلاب
+    path('', views.home, name='home'), 
+    path('Student/', views.login, name='Student'),  
+    path('Teacher/', views.LogIN_M, name='Teacher'),  
+    path('Dean-of-Student-office/', views.LogInDeanStu, name='Dean_of_Student_office') 
 ]

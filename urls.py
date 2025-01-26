94ab85e4bfb85ed37a0b8761742fbe44f5c68c5b
sham621 <ShaimNa@ac.sce.ac.il>
2025-01-26 16:41:02 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # الصفحة الرئيسية
-    path('Student/', views.LogIn_Student, name='Student'),  # تسجيل دخول الطالب
-    path('Teacher/', views.LogIN_M, name='Teacher'),  # تسجيل دخول المعلم
-    path('Dean-of-Student-office/', views.LogInDeanStu, name='Dean_of_Student_office')  # تسجيل دخول مكتب عمادة شؤون الطلاب
-]

232ae98e28133995602b70d5966b30d8dc31c347
marwadiab <marwadi@ac.sce.ac.il>
2025-01-26 16:44:03 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'), 
-    path('Student/', views.login, name='Student'),  
-    path('Teacher/', views.login_M, name='Teacher'),  
-    path('Dean-of-Student-office/', views.LogInDeanStu, name='Dean_of_Student_office') 
-]
